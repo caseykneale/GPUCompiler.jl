f50aadeaf22b5f0607ed99f294ac35d94afd75c3
Tim Besard <tim@juliacomputing.com>
2021-02-15 10:35:51 +0100

    Initial macro-based API for registering overrides.

--- wip.jl
+++ wip.jl
@@ -7,12 +7,9 @@ original() = 0
 kernel() = original()
 
 replaced() = 42
-GPUCompiler.add_override!(GPUCompiler.CI_CACHE, original, replaced)
+GPUCompiler.@override GPUCompiler.CI_CACHE original() replaced
 
 function main()
-    @show kernel()
-
-    empty!(GPUCompiler.CI_CACHE.dict)
     native_code_llvm(kernel, Tuple{}; debuginfo=:none)
 end
 
