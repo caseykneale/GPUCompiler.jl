d2e8fe1b46c52beedc8abfb9d0d8a5d4a43b05a6
Tim Besard <tim@juliacomputing.com>
2021-02-15 12:01:39 +0100

    Allow downstream replacement of the CI cache.

--- wip.jl
+++ wip.jl
@@ -7,7 +7,7 @@ original() = 0
 kernel() = original()
 
 replaced() = 42
-GPUCompiler.@override GPUCompiler.CI_CACHE original() replaced
+GPUCompiler.@override GPUCompiler.GLOBAL_CI_CACHE original() replaced
 
 function main()
     native_code_llvm(kernel, Tuple{}; debuginfo=:none)
